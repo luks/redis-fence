d85766462d7a2ac261ef58247bed186d381e6a83
Luka Musin <luka.musin@firma.seznam.cz>
2018-03-21 15:32:31 +0100

    Parser ready

--- redis-fence.c
+++ redis-fence.c
@@ -23,7 +23,7 @@ typedef struct r_cmd {
     struct r_cmd *next;
 } r_cmd_t;
 
-typedef struct r_buff {
+typedef struct r_buffer {
     size_t pos;
     char *buff;
     int cmd_cnt;
@@ -31,11 +31,11 @@ typedef struct r_buff {
 } r_buff_t;
 
 void cmd_free(struct r_cmd *cmd);
-void cmd_print(struct r_cmd *n);
+void cmd_print(struct r_cmd *cmd);
 void cmd_log(struct r_cmd *cmd);
 int cmd_state_len(struct r_cmd *cmd, char *sb);
-struct r_buff *buffer_new(char *buff);
-int cmd_parse(struct r_cmd *cmd, struct r_buff *pBuff);
+struct r_buffer *buffer_new(char *buff);
+int cmd_parse_recursive(struct r_cmd *cmd, struct r_buffer *pBuff);
 
 #endif
 
@@ -78,10 +78,10 @@ int cmd_state_len(struct r_cmd *cmd, char *sb) {
     return STATE_FAIL;
 }
 
-struct r_buff *buffer_new(char *buff) {
+struct r_buffer *buffer_new(char *buff) {
 
-    struct r_buff *r_buff;
-    r_buff = calloc(1, sizeof(struct r_buff));
+    struct r_buffer *r_buff;
+    r_buff = calloc(1, sizeof(struct r_buffer));
     r_buff->buff = buff;
     return r_buff;
 }
@@ -96,7 +96,7 @@ void cmd_free_all(struct r_cmd *cmd) {
     }
 }
 
-void buffer_free(struct r_buff * buffer) {
+void buffer_free(struct r_buffer *buffer) {
 
     cmd_free_all(buffer->cmd);
     free(buffer);
@@ -115,45 +115,7 @@ void cmd_free(struct r_cmd *cmd) {
     }
 }
 
-int cmd_parse(struct r_cmd *cmd, struct r_buff *pBuff) {
-
-    int i;
-    char sb[BUF_SIZE] = {0};
-    char *ptr, *ptr1;
-
-    if (cmd_state_len(cmd, sb) != STATE_CONTINUE) {
-        printf("\r\rargc format error:%s", sb);
-        return false;
-    }
-    cmd->argc = (size_t) strtol(sb, &ptr, 0);
-    cmd->argv  =(char**)calloc(cmd->argc, sizeof(char*));
-
-    for (i = 0; i < cmd->argc; i++) {
-        size_t argv_len;
-        char *v;
-        memset(sb, 0, BUF_SIZE);
-        if (cmd_state_len(cmd, sb) != STATE_CONTINUE) {
-            printf("\r\rargv's length error, packet:%s", sb);
-            return false;
-        }
-        argv_len = (size_t) strtol(sb, &ptr1, 0);
-
-        if (argv_len < BUF_SIZE) { //todo  <--- this is product of tirednes and stupidity FIX IT!!!!
-            v = (char *) malloc(sizeof(char) * argv_len + 1);
-            memcpy(v, cmd->buff + (cmd->pos), argv_len);
-            v[argv_len] = '\0';
-            cmd->argv[i] = v;
-        }
-        cmd->pos += argv_len + 2;
-    }
-
-    pBuff->pos += cmd->pos;
-    pBuff->cmd_cnt += 1;
-
-    return (int) ((pBuff->buff + pBuff->pos)[0] == *REDIS_STAR ? STATE_CONTINUE : STATE_FAIL);
-}
-
-int cmd_parse_recursive(struct r_cmd *cmd, struct r_buff *pBuff) {
+int cmd_parse_recursive(struct r_cmd *cmd, struct r_buffer *pBuff) {
 
     cmd->buff = pBuff->buff+pBuff->pos;
 
@@ -198,7 +160,7 @@ int cmd_parse_recursive(struct r_cmd *cmd, struct r_buff *pBuff) {
 }
 
 
-void buffer_parse(struct r_buff * buffer) {
+void buffer_parse(struct r_buffer * buffer) {
 
     buffer->cmd = calloc(1, sizeof(struct r_cmd));
     cmd_parse_recursive(buffer->cmd, buffer);
@@ -223,19 +185,23 @@ void cmd_log(struct r_cmd *cmd) {
     }
 }
 
-int main(int argc, char *argv[]) {
+void test_parser(char * data) {
 
-//    char * data = "*3\r\n$4\r\nhget\r\n$7\r\nprofile\r\n$10\r\nakul.musis\r\n*3\r\n$4\r\ntegh\r\n$7\r\nprofile\r\n$10\r\nluka.musin\r\n*3\r\n$4\r\ntttt\r\n$7\r\nprofile\r\n$10\r\nluka.musin\r\n";
-//    char * data = "*3\r\n$4\r\nhget\r\n$7\r\nprofile\r\n$10\r\nakul.musin\r\n";
-
-    char * data = "*3\r\n$4\r\nsrem\r\n$28\r\ntarget:owner(luka.musin).idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$18\r\ntarget:realm().idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$20\r\ntarget:tag(keks).idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$20\r\ntarget:tag(poks).idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$21\r\ntarget:hostname().idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$26\r\ntarget:template(false).idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$10\r\ntarget.idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nhdel\r\n$6\r\ntarget\r\n$5\r\nkoloe\r\n*2\r\n$4\r\nincr\r\n$8\r\nrevision\r\n*1\r\n$4\r\nexec\r\n";
-
-
-    struct r_buff *buffer = buffer_new(data);
+    struct r_buffer *buffer = buffer_new(data);
     buffer_parse(buffer);
+    cmd_print(buffer->cmd);
+    printf("\r\n");
     printf("Buffer position: [%d]\n", (int) buffer->pos);
     printf("Commands count: [%d]\n", (int) buffer->cmd_cnt);
-    cmd_print(buffer->cmd);
+    printf("_____________________________________________________\n");
     buffer_free(buffer);
+}
+
+int main(int argc, char *argv[]) {
+
+    test_parser("*3\r\n$4\r\nhget\r\n$7\r\nprofile\r\n$10\r\nakul.musis\r\n*3\r\n$4\r\ntegh\r\n$7\r\nprofile\r\n$10\r\nluka.musin\r\n*3\r\n$4\r\ntttt\r\n$7\r\nprofile\r\n$10\r\nluka.musin\r\n");
+    test_parser("*3\r\n$4\r\nhget\r\n$7\r\nprofile\r\n$10\r\nakul.musin\r\n");
+    test_parser("*3\r\n$4\r\nsrem\r\n$28\r\ntarget:owner(luka.musin).idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$18\r\ntarget:realm().idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$20\r\ntarget:tag(keks).idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$20\r\ntarget:tag(poks).idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$21\r\ntarget:hostname().idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$26\r\ntarget:template(false).idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nsrem\r\n$10\r\ntarget.idx\r\n$5\r\nkoloe\r\n*3\r\n$4\r\nhdel\r\n$6\r\ntarget\r\n$5\r\nkoloe\r\n*2\r\n$4\r\nincr\r\n$8\r\nrevision\r\n*1\r\n$4\r\nexec\r\n");
+    test_parser("*3\r\n$11\r\nsinterstore\r\n$29\r\ntmp_sinterstore@1521640901:94\r\n$10\r\ntarget.idx\r\n");
 
 }
