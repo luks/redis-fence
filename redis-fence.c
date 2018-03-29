766bc10c769fd543b12985cfc2bfe7282875e32d
Luka Musin <luka.musin@firma.seznam.cz>
2018-03-30 00:54:58 +0200

    This is the way to do it

--- redis-fence.c
+++ redis-fence.c
@@ -1,209 +1,189 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
 
 #ifndef _REQUEST_H
 #define _REQUEST_H
-#define BUF_SIZE (1024*16)
-#define REDIS_STAR  "*"
-#define REDIS_DOLLAR  "$"
-#define REDIS_CR  "\r"
-#define REDIS_LF  "\n"
+
+struct request{
+    char *querybuf;
+    int argc;
+    char **argv;
+    size_t pos;
+};
+
+struct request *request_new(char *querybuf);
+int  request_parse(struct request *request);
+void request_free(struct request *request);
+void request_dump(struct request *request);
+
+#endif
+
+
+#define BUF_SIZE (1024*10)
+
+#define REDIS_STAR '*'
 
 enum {
     STATE_CONTINUE,
-    STATE_FAIL,
+    STATE_FAIL
 };
 
-struct r_cmd {
-    size_t pos;
-    char *buff;
-    size_t argc;
-    char **argv;
-    struct r_cmd *next;
+enum {
+    OK,
+    FAILED
 };
 
-struct r_buffer {
-    char *buff;
-    struct r_cmd *cmd;
-};
-
-
-void cmd_log(struct r_cmd *cmd);
-struct r_buffer *buffer_new(char *buff);
-
-struct r_buffer *buffer_new(char *buff);
-void test_parser(char * data);
-void buffer_free(struct r_buffer *buffer);
-#endif
-
-void cmd_print(struct r_cmd *cmd) {
-    while (cmd != NULL) {
-        printf("\r\n");
-        cmd_log(cmd);
-        cmd = cmd->next;
-    }
-}
-
-void cmd_log(struct r_cmd *cmd) {
-    int i;
-    if (cmd == NULL)
-        return;
-    printf("\nargc:<%d>\n", (int) cmd->argc);
-    for (i = 0; i < cmd->argc; i++) {
-        printf("\nargv[%d]:<%s>\n",i,cmd->argv[i]);
-    }
+struct request *request_new(char *querybuf) {
+    struct request *req;
+    req=calloc(1, sizeof(struct request));
+    req->querybuf = querybuf;
+    return req;
 }
 
 
-void buffer_free(struct r_buffer *buffer) {
-
-    int i;
-    if (buffer->cmd) {
-        for (i = 0; i < buffer->cmd->argc; i++) {
-            if (buffer->cmd->argv[i])
-                free(buffer->cmd->argv[i]);
-        }
-        //free(buffer->cmd->argv);
-        free(buffer->cmd);
-    }
-    free(buffer);
-}
-
-struct r_buffer *buffer_new(char *buff) {
-
-    struct r_buffer *r_buff;
-    r_buff = calloc(1, sizeof(struct r_buffer));
-    r_buff->buff = buff;
-    return r_buff;
-}
-
-
-int state_machine(struct r_cmd *cmd, char *sb) {
-
-    int term = 0, first = 0;
+int req_state_len(struct request *req,char *sb) {
+    int term=0, first=0;
     char c;
-    size_t i = 0;
-    size_t pos = i;
-    while((c = cmd->buff[i]) != '\0') {
+    int i = req->pos;
+    int pos=i;
+
+    while((c = req->querybuf[i]) != '\0') {
         first++;
         pos++;
         switch(c) {
             case '\r':
                 term = 1;
                 break;
+
             case '\n':
                 if (term) {
-                    cmd->pos = pos;
+                    req->pos = pos;
                     return STATE_CONTINUE;
                 }
                 else
                     return STATE_FAIL;
             default:
                 if (first == 1) {
-                    if (c != *REDIS_DOLLAR &&
-                        c != *REDIS_STAR &&
-                        c != *REDIS_CR &&
-                        c != *REDIS_LF)
+                    /* the first symbol is not '*' or '$'*/
+                    if (c != '$' && c != '*' && c != '\r' && c != '\n')
                         return STATE_FAIL;
                 } else {
-                    if (c >= '0' && c <= '9') {
+                    /* the symbol must be numeral*/
+                    if (c >= '0' && c <= '9')
                         *sb++ = c;
-                    } else {
+                    else
                         return STATE_FAIL;
-                    }
                 }
                 break;
         }
         i++;
     }
+
     return STATE_FAIL;
 }
 
 
-int test_command(struct r_cmd *cmd) {
-
+int request_parse(struct request *req) {
+    int i;
     char sb[BUF_SIZE] = {0};
 
-    printf( "Buffer will be sent to state machine\n");
-
-    if(state_machine(cmd, sb) == STATE_CONTINUE) {
-        // we expect that first state will be digit
-        if(isdigit(*sb)) {
-            printf( "So far so good [%s]\n",  sb);
-            printf( "Buffer will be processed\n");
-
-            //processing buffer
-            char *ptr;
-            int i;
-
-
-            cmd->argc = (size_t) strtol(sb, &ptr, 0);
-            cmd->argv  =(char**)calloc(1, sizeof(char*));
-
-            for (i = 0; i < 1; i++) {
-                size_t argv_len;
-                char *v;
-                memset(sb, 0, BUF_SIZE);
-
-                if (state_machine(cmd, sb) != STATE_CONTINUE) {
-                    continue;
-                }
-                argv_len = (size_t) strtol(sb, &ptr, 0);
-
-                printf("Argument length [%d] \n", (int) argv_len);
-
-                v = (char *) malloc(sizeof(char) * argv_len + 1);
-                memcpy(v, cmd->buff + (cmd->pos), argv_len);
-                v[argv_len] = '\0';
-
-                printf("Command should be [%s]\n", v);
-
-                cmd->argv[i] = v;
-                cmd->pos += argv_len + 2;
-
-            }
-            // processing buffer end
-        }
-    } else {
-        printf("Not so good [%s]\n", sb);
+    if (req_state_len(req, sb) != STATE_CONTINUE) {
+        fprintf(stderr, "argc format ***ERROR***,packet:%s\n", sb);
+        return FAILED;
     }
+    req->argc = atoi(sb);
 
-    return 1;
+    req->argv  =(char**)calloc(req->argc, sizeof(char*));
+    for (i = 0; i < req->argc; i++) {
+        int argv_len;
+        char *v;
 
+        /*parse argv len*/
+        memset(sb, 0, BUF_SIZE);
+        if (req_state_len(req, sb) != STATE_CONTINUE) {
+            fprintf(stderr, "argv's length format ***ERROR***,packet:%s\n", sb);
+            return FAILED;
+        }
+        argv_len=atoi(sb);
+
+        /*get argv*/
+        v=(char*)malloc(sizeof(char) * argv_len);
+        memcpy(v, req->querybuf+(req->pos), argv_len);
+        req->argv[i] = v;
+        req->pos += argv_len+2;
+    }
+    return OK;
 }
 
 
-int inspect_buffer(struct r_buffer *pBuff) {
+int request_multiple_parse(char *data) {
 
     int i = 0;
+
     char c;
 
-    while((c = pBuff->buff[i]) != '\0') {
+    while((c = data[i]) != '\0') {
 
-        if (c == *REDIS_STAR) {
+        if (c == REDIS_STAR) {
 
             printf("Star found at position [%d]\n", i);
 
-            pBuff->cmd = calloc(1, sizeof(struct r_cmd));
-            pBuff->cmd->buff = &pBuff->buff[i];
+            struct request *req = request_new(&data[i]);
 
-            printf( "Buffer that will be sent to test method\n");
-            test_command(pBuff->cmd);
+            if(request_parse(req) != OK) {
+                printf("Error parse request at position [%d] \n", i);
+            };
+
+            // jump over bytes already parsed
+            if(req->pos > 1) {
+                i += (int) req->pos - 1;
+            }
+
+            request_dump(req);
+            request_free(req);
 
         }
+        printf("Current position [%d]\n", i);
         i++;
     }
-    return 1;
+
 }
 
-void test_parser(char * data) {
 
-    struct r_buffer *buffer = buffer_new(data);
-    inspect_buffer(buffer);
-    buffer_free(buffer);
+void request_dump(struct request *req) {
+    int i;
+    if (req == NULL)
+        return;
+
+    printf("request-dump--->");
+    //printf("request-tail--->[%s]", req->querybuf);
+    printf("argc:<%d>\n", req->argc);
+    for (i = 0; i < req->argc; i++) {
+        printf("argv[%d]:<%s>\n",i,req->argv[i]);
+    }
+    printf("\n");
+}
+
+void request_free(struct request *req) {
+    int i;
+    if (req) {
+        for (i = 0; i < req->argc; i++) {
+            if (req->argv[i])
+                free(req->argv[i]);
+        }
+        free(req->argv);
+        free(req);
+    }
+}
+
+
+void test_parser(char * data) {
+    //struct request *buffer = request_new(data);
+    request_multiple_parse(data);
+    //request_dump(buffer);
+    //request_free(buffer);
+
 }
 
 
@@ -218,11 +198,11 @@ int main(int argc, char *argv[]) {
 //    test_parser("*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n");
 //    test_parser("*2\r\n$4720\r\ndasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd\r\n$1\r\n*\r\n");
 //    test_parser("*2\r\n$4\r\nkeys\r\n$4720\r\ndasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdadasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
-//    test_parser("*3\r\n$4\r\nhget\r\n$7\r\nprofile\r\n$10\r\nakul.musis\r\n*3\r\n$4\r\ntegh\r\n$7\r\nprofile\r\n$10\r\nluka.musin\r\n*3\r\n$4\r\ntttt\r\n$7\r\nprofile\r\n$10\r\nluka.musin\r\n");
-//    test_parser("*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n");
-//    test_parser("*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n");
-    test_parser("\r\nss\rn\rnss*ssss*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n");
-    test_parser("*2\r\n$4\r\nkeys\r\n$1\r\n*3\r\n$11\r\nsinterstore\r\n$29\r\ntmp_sinterstore@1521640901:94\r\n$10\r\ntarget.idx\r\n");
+    test_parser("kolon*****\r\n$5aoklo*3\r\n$4\r\nhget\r\n$7\r\nprofile\r\n$10\r\nakul.musis\r\n*3\r\n$4\r\ntegh\r\n$7\r\nprofile\r\n$10\r\nluka.musin\r\n*3\r\n$4\r\ntttt\r\n$7\r\nprofile\r\n$10\r\nluka.musin\r\n");
+    //test_parser("*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n");
+   // test_parser("*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n");
+   // test_parser("*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkeys\r\n$1\r\n*\r\n*2\r\n$4\r\nkey");
+
 
 
 }
